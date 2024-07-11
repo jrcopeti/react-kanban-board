aadf8b6b69912ace33fa91c842cce939d4706123
Jose Copeti <jrcopeti@gmail.com>
2024-07-11 15:57:51 +0200

    sidebar animation

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -2,15 +2,18 @@ import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import Sidebar from "./Sidebar";
-
+import { useOpenSidebar } from "../hooks/useOpenSidebar";
 
 function AppLayout() {
+  const { isSidebarOpen } = useOpenSidebar();
   return (
     <>
       <div className="h-screen">
         <Navbar />
         <Sidebar />
-        <main className="h-screen">
+        <main
+          className={`h-screen p-4 duration-300 ease-in-out ${isSidebarOpen ? "translate-x-[26rem]" : "translate-x-0"}`}
+        >
           <Outlet />
           <Footer />
         </main>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,10 +4,13 @@ import { routes } from "../utils";
 import { useOpenSidebar } from "../hooks/useOpenSidebar";
 
 function Navbar() {
-  const { toggleSidebar } = useOpenSidebar();
+  const { toggleSidebar, headerRef } = useOpenSidebar();
 
   return (
-    <nav className="sticky top-0 z-50 flex items-center justify-between bg-gray-100 px-12 py-3 text-lg font-semibold">
+    <nav
+      className="sticky top-0 z-50 flex items-center justify-between bg-gray-100 px-12 py-3 text-lg font-semibold"
+      ref={headerRef}
+    >
       <button onClick={toggleSidebar}>
         <MdOutlineSpaceDashboard size={50} className="cursor-pointer" />
       </button>
