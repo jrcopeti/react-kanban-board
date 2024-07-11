aa138f9ca58c188dd5f767fb34f19d1e6a712d8d
Jose Copeti <jrcopeti@gmail.com>
2024-07-11 12:51:25 +0200

    disable eslint in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unnecessary-type-assertion */
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
