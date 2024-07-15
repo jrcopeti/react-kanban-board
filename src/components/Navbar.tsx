e8b263a5d9b226c6e6b9c6d50b1ed66a69adace4
Jose Copeti <jrcopeti@gmail.com>
2024-07-15 22:31:36 +0200

    included pathname in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,17 @@
 import { MdOutlineSpaceDashboard } from "react-icons/md";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useParams } from "react-router-dom";
 import { routes } from "../utils";
 import { useOpenSidebar } from "../hooks/useOpenSidebar";
+import { FiMenu } from "react-icons/fi";
 
 function Navbar() {
   const { toggleSidebar, headerRef } = useOpenSidebar();
+  const path = useLocation();
+  console.log("pathname", path);
+
+  const pathname =
+    path.pathname === "/" ? "home" : path.pathname.split("/").join("");
+  console.log("pathname", pathname);
 
   return (
     <nav
@@ -12,14 +19,19 @@ function Navbar() {
       ref={headerRef}
     >
       <button onClick={toggleSidebar}>
-        <MdOutlineSpaceDashboard size={50} className="cursor-pointer" />
+        <FiMenu size={40} className="cursor-pointer" />
       </button>
 
-      {routes.map((route) => (
-        <Link to={route.to} key={route.to}>
-          {route.label}
-        </Link>
-      ))}
+      <div className="capitalize">
+        <h2>{pathname}</h2>
+      </div>
+
+      <Link to="/">
+        <div className="flex cursor-pointer items-center gap-4">
+          <h1>Kanban Board</h1>
+          <MdOutlineSpaceDashboard size={50} />
+        </div>
+      </Link>
     </nav>
   );
 }
