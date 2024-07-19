24009745649ecb80fd5aa5114a82be96afa6645e
Jose Copeti <jrcopeti@gmail.com>
2024-07-19 09:17:39 +0200

    due date

--- src/components/task/DueDateInMouseIsOver.tsx
+++ src/components/task/DueDateInMouseIsOver.tsx
@@ -4,19 +4,20 @@ import { useTask } from "../../hooks/useTask";
 function DueDateInMouseIsOver() {
   const { task } = useTask();
   const { dueDate } = task;
+
   return (
     <section
       title="Due Date"
       className={
         dueDate
-          ? `${isToday(new Date(dueDate)) ? "right-[5rem]" : "right-[1.5rem]"} absolute bottom-2 z-30 -translate-x-1/2 flex-col gap-1 text-lg`
+          ? `${isToday(new Date(dueDate)) ? "right-[3rem] text-rose-500 dark:text-rose-900" : "right-[1.5rem]"} absolute bottom-2 z-30 -translate-x-1/2 flex-col gap-1 text-lg text-pallette-300 dark:text-slate-500`
           : ""
       }
     >
-      <p className="text-left text-xs text-pallette-300 dark:text-slate-500">
+      <p className="text-left text-xs">
         {dueDate
           ? isToday(new Date(dueDate))
-            ? "Today"
+            ? "Due: Today"
             : `Due: ${formatDistanceToNow(new Date(dueDate), { addSuffix: true })}`
           : ""}
       </p>
